364864af75d361be54ea1da8e34ec99e51645cc8
Kevin Portugal <portugalkevin@gmail.com>
2015-06-15 00:14:38 -0700

    pass about test

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -9,6 +9,11 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get help" do
     get :help
     assert_response :success
+  end 
+  
+  test "should get about" do
+    get :about
+    assert_response :success
   end
 
 end
